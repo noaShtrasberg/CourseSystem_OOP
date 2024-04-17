ecd66eb9d5cf8ec2abfa11851d1e1cda7ee01093
noaShtrasberg <155996037+noaShtrasberg@users.noreply.github.com>
2024-04-17 19:42:14 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,19 +4,20 @@ public class Main {
         Student Noa = Ariel.signUp_student("Noa", "12345");
         Ariel.logIn(Noa);
         Staff Eran = Ariel.signUp_staff("Eran", "12345", "Lecturer");
-        Course OOP3 = Ariel.createCourseSystem(Eran, "OOP", 2468, 2, "Seminar");
+        Course OOP3 = Ariel.createCourseSystem(Eran, "OOP", 2468, 2, "Seminar"); // Eran is not connect
         Ariel.logIn(Eran);
         Course OOP = Ariel.createCourseSystem(Eran, "OOP", 2468, 2, "Seminar");
         Course SP = Ariel.createCourseSystem(Eran, "SP", 3579, 2, "Required");
-        Course OOP2 = Ariel.createCourseSystem(Eran, "OOP2", 2468, 2, "Seminar");
+        Course OOP2 = Ariel.createCourseSystem(Eran, "OOP2", 2468, 2, "Optional");
+        System.out.println(OOP2.type); // same ID like OOP, should print the type of OOP - Seminar
         Ariel.signCourse(Noa, OOP);
         Student Hagit = Ariel.signUp_student("Hagit", "12345");
         Student Yair = Ariel.signUp_student("Yair", "12345");
         Ariel.logIn(Hagit);
         Ariel.logIn(Yair);
         Ariel.signCourse(Hagit, OOP);
-        Ariel.signCourse(Yair, OOP);
+        Ariel.signCourse(Yair, OOP); // OOP is full. ask Yair if he wants to sign for updates
         Ariel.unSignCourse(Noa, OOP);
-        Ariel.signCourse(Yair, OOP);
+        Ariel.signCourse(Yair, OOP); // now Yair can sign to OOP
     }
 }
